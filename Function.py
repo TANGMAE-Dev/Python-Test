c252296f472c11bfcee26b432e273abc001c2205
TangmaeDev <ttmm2352@gmail.com>
2025-07-25 08:46:39 +0700

    Update Function.py

--- Function.py
+++ Function.py
@@ -1,4 +1,4 @@
-def SaveEmployee(name,department,salary=15000):
+def SaveEmployee(name,department,salary=15000):#ฟังค์ชั่นรับค่าparamiterมาใช้ด้านล่าง
     print("----------------------------------")
     print(f"ชื่อ {name} , ตำแหน่ง {department}")
     print(f"เงินเดือน {salary} บาท")
